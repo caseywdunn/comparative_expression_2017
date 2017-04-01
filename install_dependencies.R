3e645a7a17be376a32432ef2cb1a53019cb10adf
Casey Dunn <casey_dunn@brown.edu>
2017-04-01 18:25:20 -0400

    use all available cores

--- install_dependencies.R
+++ install_dependencies.R
@@ -1,5 +1,10 @@
 # Installs manuscript dependencies
-
+# 
+# This installs all dependencies needed to run the 
+# manuscript, as verified by running it in a 
+# container based on the docker image 
+# https://hub.docker.com/r/rocker/tidyverse/
+#
 # From CRAN
 install.packages( 
 	c(
@@ -17,10 +22,10 @@ install.packages(
 )
 
 # From Bioconductor, needed for ggtree and treeio
-source("https://bioconductor.org/biocLite.R")
+source( "https://bioconductor.org/biocLite.R" )
 biocLite()
 
 # From github
-devtools::install_github( "GuangchuangYu/treeio" )
-devtools::install_github( "GuangchuangYu/ggtree" )
-devtools::install_github( "caseywdunn/hutan" )
+devtools::install_github( "GuangchuangYu/treeio", quiet=TRUE )
+devtools::install_github( "GuangchuangYu/ggtree", quiet=TRUE )
+devtools::install_github( "caseywdunn/hutan", quiet=TRUE )
